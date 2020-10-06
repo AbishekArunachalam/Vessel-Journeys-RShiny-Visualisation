8b5bda3896f8095cf8ab87baba91b3ef90bc1057
Abishek Arunachalam <abs.abs105@gmail.com>
2020-10-06 18:08:37 +1100

    Changes to server.R file

--- src/server.R
+++ src/server.R
@@ -3,8 +3,9 @@ library(shiny)
 library(shinyWidgets)
 library(leaflet)
 
-# setwd('~/Documents/AppsilonDataScienceTask/')
+#setwd('~/Documents/Appsilon/')
 options(shiny.maxRequestSize=550*1024^2)
+options(warn = -1)
 
 ships <- read.csv('./ships.csv')
 dim(ships) # 3,102,887 rows, 26 columns
@@ -17,12 +18,22 @@ dim(ships) # 2,996,023 rows
 ship_type <- c('Cargo', 'Tanker', 'Tug', 'Fishing', 'Passenger', 'Pleasure', 'Navigation', 'High Special')
 names(ship_type) <- c('Cargo', 'Tanker', 'Tug', 'Fishing', 'Passenger', 'Pleasure', 'Navigation', 'High Special')
 
-calculate_max_distance <- function(vessel_type, vessel_name){
+findJourney <- function(vessel_type, vessel_name){
+  ############################################
+  # calcuate max distance covered by each vessel 
+  # and the source and destination of journey
+  
+  # param:
+  # vessel_type - type of vessel
+  # vessel_name - name of vessel
+  # return:
+  # max distance covered and the journey dataframe
+  ############################################
   ships <- ships %>%
     filter(ship_type == vessel_type & SHIPNAME == vessel_name)
   num_rows <- nrow(ships)
   max_distance_covered = 0
-  out = list()
+  output = list()
   time_taken_buffer = 0
   for(row in 1:(num_rows-1)){
     start_time <- strptime(ships[row, 'DATETIME'], format = "%Y-%m-%d %H:%M:%S", tz="Poland")
@@ -30,16 +41,18 @@ calculate_max_distance <- function(vessel_type, vessel_name){
     speed = (ships[row, "SPEED"]) # convert speed from m/hr to m/sec
     time_taken <- as.numeric(difftime(end_time, start_time, units = "secs")) # calculate time difference in seconds
     distance <- (speed * time_taken) # calculate distance in meters
-    Sys.sleep(0.001)
+    
     if(distance >= max_distance_covered){
-      if(row == (num_rows-2) & length(out) == 0){
+      # special case: some vessels have not moved
+      if(row == (num_rows-1) & length(output) == 0){
         source <- ships[row, c('LON','LAT')]
         destination <- ships[row+1, c('LON','LAT')]
         journey <- rbind(source, destination)
         colnames(journey) = c('long', 'lat')
-        out$journey <- journey
-        out$max_distance_covered <- distance
+        output$journey <- journey
+        output$max_distance_covered <- distance
       }
+      # case: if distances are equal and time taken is more skip loop
       if(distance == max_distance_covered & (time_taken > time_taken_buffer)){
         next
       }
@@ -48,25 +61,23 @@ calculate_max_distance <- function(vessel_type, vessel_name){
       journey <- rbind(source, destination)
       colnames(journey) = c('long', 'lat')
       time_taken_buffer = time_taken
-      out$journey <- journey
-      out$max_distance_covered <- distance
+      output$journey <- journey
+      output$max_distance_covered <- distance
     }
   }
-  return(out)
+  return(output)
 }
-
 
 server <- function(input, output) {
   
-  #Sys.sleep(0.2)
   output$ship_type <- renderUI({
     selectInput("vessel_type_dropdown", "", 
                 choices = ship_type,
                 selected = "High Special")
   })
+  
   output$ship_name <- renderUI({
-    #req(input$vessel_type_dropdown)
-    validate(need(length(input$vessel_type_dropdown)>0, ""))
+    req(input$vessel_type_dropdown)
     selectInput("vessel_name_dropdown", "", 
                 choices = unique(ships[ships$ship_type == input$vessel_type_dropdown,'SHIPNAME']),
                 selected = "VALO")
@@ -74,26 +85,18 @@ server <- function(input, output) {
   
   max_journey <- reactive({
     req(input$vessel_type_dropdown, input$vessel_name_dropdown)
-    #validate(need(length(input$vessel_type_dropdown)>0, ""))
-    #validate(need(length(input$vessel_name_dropdown)>0, ""))
-    max_journey <- calculate_max_distance(input$vessel_type_dropdown, input$vessel_name_dropdown)
+    max_journey <- findJourney(input$vessel_type_dropdown, input$vessel_name_dropdown)
     max_journey
   })
   
   output$distance_covered <- renderText({
-    # validate(need(length(input$vessel_type_dropdown)>0, ""))
-    # validate(need(!is.na(max_journey()$max_distance_covered)), "")
-    #req(input$vessel_type_dropdown, max_journey()$max_distance_covered)
-    if(is.null(max_journey()))return()
-    paste0("The distance covered by ", input$vessel_name_dropdown, " is ", toString(max_journey()$max_distance_covered), " meters.")
-    
+    req(input$vessel_type_dropdown, max_journey()$max_distance_covered)
+    paste0("The distance covered by ", input$vessel_name_dropdown, " is ", 
+           toString(max_journey()$max_distance_covered), " meters.")
   })
   
   output$mymap <- renderLeaflet({
-    if(is.null(max_journey()$journey))return()
-    #req(max_journey()$journey)
-    #validate(need(!is.na(max_journey()$journey)), "")
-    #if(!is.na(max_journey()$journey)){
+    req(max_journey()$journey)
     leaflet(data = max_journey()$journey) %>%
       addTiles() %>%
       addProviderTiles(providers$CartoDB.DarkMatterNoLabels, group = "Carto DB dark") %>%
@@ -112,3 +115,4 @@ server <- function(input, output) {
       addLayersControl(baseGroups = c("Carto DB Positron","Carto DB dark"))
   })
 }
+
