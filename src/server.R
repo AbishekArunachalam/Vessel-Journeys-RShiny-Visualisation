49d4c2647e1a62cb8966eb98ac1676eed18ada65
Abishek Arunachalam <abs.abs105@gmail.com>
2020-10-06 17:31:05 +1100

    Changes to server.R file

--- src/server.R
+++ src/server.R
@@ -54,3 +54,61 @@ calculate_max_distance <- function(vessel_type, vessel_name){
   }
   return(out)
 }
+
+
+server <- function(input, output) {
+  
+  #Sys.sleep(0.2)
+  output$ship_type <- renderUI({
+    selectInput("vessel_type_dropdown", "", 
+                choices = ship_type,
+                selected = "High Special")
+  })
+  output$ship_name <- renderUI({
+    #req(input$vessel_type_dropdown)
+    validate(need(length(input$vessel_type_dropdown)>0, ""))
+    selectInput("vessel_name_dropdown", "", 
+                choices = unique(ships[ships$ship_type == input$vessel_type_dropdown,'SHIPNAME']),
+                selected = "VALO")
+  })
+  
+  max_journey <- reactive({
+    req(input$vessel_type_dropdown, input$vessel_name_dropdown)
+    #validate(need(length(input$vessel_type_dropdown)>0, ""))
+    #validate(need(length(input$vessel_name_dropdown)>0, ""))
+    max_journey <- calculate_max_distance(input$vessel_type_dropdown, input$vessel_name_dropdown)
+    max_journey
+  })
+  
+  output$distance_covered <- renderText({
+    # validate(need(length(input$vessel_type_dropdown)>0, ""))
+    # validate(need(!is.na(max_journey()$max_distance_covered)), "")
+    #req(input$vessel_type_dropdown, max_journey()$max_distance_covered)
+    if(is.null(max_journey()))return()
+    paste0("The distance covered by ", input$vessel_name_dropdown, " is ", toString(max_journey()$max_distance_covered), " meters.")
+    
+  })
+  
+  output$mymap <- renderLeaflet({
+    if(is.null(max_journey()$journey))return()
+    #req(max_journey()$journey)
+    #validate(need(!is.na(max_journey()$journey)), "")
+    #if(!is.na(max_journey()$journey)){
+    leaflet(data = max_journey()$journey) %>%
+      addTiles() %>%
+      addProviderTiles(providers$CartoDB.DarkMatterNoLabels, group = "Carto DB dark") %>%
+      addCircleMarkers(lng = ~long, 
+                       lat = ~lat, 
+                       radius = 5, 
+                       fillOpacity = 0.5,
+                       weight = 2,
+                       opacity = 0.8, 
+                       color = "red") %>%
+      addPolylines(lat = ~lat, 
+                   lng = ~long, 
+                   weight = 1, 
+                   opacity = 0.5, 
+                   color = "#820a0a") %>%
+      addLayersControl(baseGroups = c("Carto DB Positron","Carto DB dark"))
+  })
+}
